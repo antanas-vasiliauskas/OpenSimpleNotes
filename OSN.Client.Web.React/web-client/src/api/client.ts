91626b9cd099d1a002ced70d6cdf2fee4d57a919
Vasiliauskas Antanas <antanas.vasiliauskas@mif.stud.vu.lt>
2025-08-05 22:05:53 +0300

    bugfix: uses env variables

--- OSN.Client.Web.React/web-client/src/api/client.ts
+++ OSN.Client.Web.React/web-client/src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "https://localhost:44398/api",
+    baseURL: process.env.REACT_APP_API_URL,
 });
 
 api.interceptors.request.use(config => {
