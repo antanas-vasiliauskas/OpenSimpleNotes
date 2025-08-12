fc1f31dd97d98c2c3bf8d53a906890ed42ba2f67
Vasiliauskas Antanas <antanas.vasiliauskas@mif.stud.vu.lt>
2025-08-12 11:54:51 +0300

    reverting to Bearer Tokens

---
+++ OSN.Infrastructure/Authentication/AuthenticationConstants.cs
@@ -0,0 +1,7 @@
+namespace OSN.Infrastructure.Authentication;
+
+public static class AuthenticationConstants
+{
+    public const string AuthTokenCookieName = "auth-token";
+    public const string DefaultScheme = "JwtCookie";
+}
