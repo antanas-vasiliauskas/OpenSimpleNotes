f8e5df5f62bdd3545b14b42c6eedcd675ce672b3
Vasiliauskas Antanas <antanas.vasiliauskas@mif.stud.vu.lt>
2025-10-07 00:41:37 +0300

    feat: production android build 1.3.0, new app icon

--- OSN.Client.Mobile.Android/app/build.gradle.kts
+++ OSN.Client.Mobile.Android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.alpharoot.opensimplenotes"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
